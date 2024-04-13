99e02f887a5e4d56fadd6b23eb7d1aa78da03c65
김연신 <127587330+YeonShin@users.noreply.github.com>
2024-04-14 00:55:36 +0900

    init

---
+++ cbnu2/app/src/main/java/com/example/cbnu2/drawingapp/DrawingAppActivity.java
@@ -0,0 +1,16 @@
+package com.example.cbnu2.drawingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cbnu2.R;
+
+public class DrawingAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing_app);
+    }
+}
---
+++ cbnu2/app/src/main/java/com/example/cbnu2/drawingapp/DrawingView.java
@@ -0,0 +1,14 @@
+package com.example.cbnu2.drawingapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class DrawingView extends View {
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
+
